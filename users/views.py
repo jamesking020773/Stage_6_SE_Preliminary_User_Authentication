5a728e7abb433447bba3a76319e4a67cd2f25309
James King <151979414+jamesking020773@users.noreply.github.com>
2024-03-18 18:41:07 +1100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("users:login"))
+    return render(request, "users/user.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("users:index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials."
+            })
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Successfully logged out.")
+    return redirect(reverse('users:login'))
