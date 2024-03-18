1f5c38c10547db5d428c1e8e666c7deb8292a13e
James King <151979414+jamesking020773@users.noreply.github.com>
2024-03-18 20:48:02 +1100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def index(request):
     if not request.user.is_authenticated:
@@ -18,9 +19,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("users:index"))
         else:
-            return render(request, "users/login.html)
+            messages.success(request, "Invalid Credentials.")
+            return render(request, "users/login.html")
     return render(request, "users/login.html")
 
 def logout_view(request):
     logout(request)
+    messages.success(request, "Successfully logged out.")
     return redirect(reverse('users:login'))
